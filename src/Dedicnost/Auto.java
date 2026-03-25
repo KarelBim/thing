840c3ccfc304066d715590bfb60dda7f2aa1bd0b
bimkar95 <bimkar95@gasos-ro.cz>
2026-03-25 13:13:10 +0100

    f

--- src/Dedicnost/Auto.java
+++ src/Dedicnost/Auto.java
@@ -1,4 +1,28 @@
 package Dedicnost;
 
-public class Auto {
+
+public class Auto extends ZavodniVozidlo{
+    private int pocetDveri;
+    public Auto(String znacka,String barva,int rychlostmax,int pocetDveri) {
+        super(znacka,barva,rychlostmax);
+        if(pocetDveri<0){
+            pocetDveri=0;
+        }
+        this.pocetDveri = pocetDveri;
+    }
+
+    public String getInfo() {
+        return super.getInfo() + " " + pocetDveri;
+    }
+    public String zavod(){
+        return getZnacka() + " jede pomalu";
+    }
+    public int getPocetDveri() {
+        return pocetDveri;
+    }
+
+    public void info() {
+        super.info();
+        System.out.println(" počet dveří: "+ pocetDveri);
+    }
 }
